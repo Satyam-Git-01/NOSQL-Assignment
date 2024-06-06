f39cea72179d9599209674cd8a3cacc84a60d0eb
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-06-06 15:29:35 +0530

    Mongoose Setup

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello NOSQL")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const Mongoose = require("mongoose");
+
+const PORT_NUMBER = process.env.PORT_NUMBER || 5600;
+const CONNECTION_STRING = process.env.CONNECTION_STRING || null;
+
+Mongoose.connect(CONNECTION_STRING)
+  .then((response) => {
+    console.log("Database connection successfull!");
+  })
+  .catch((err) => {
+    console.log("Error while connecting DB");
+  });
+
+app.listen(PORT_NUMBER, () => {
+  console.log(`APP is running on PORT :${PORT_NUMBER}`);
+});
